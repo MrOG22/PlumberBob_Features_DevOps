f4ba8af706d72ebade699da76a6bd5314b7dbc88
Simon Stormes <37613057+StormesCraft@users.noreply.github.com>
2022-02-24 13:00:29 +0100

    Deleted Non-destructable block

--- Assets/Scripts/BrickBlock.cs
+++ Assets/Scripts/BrickBlock.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BrickBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Breaks the block
+    /// </summary>
+    public void BreakBlock()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.SetActive(false);
     }
 }
